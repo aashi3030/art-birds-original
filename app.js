5c2ebabc629e2ae6ac69ab83df208a9fba7e0207
Aashi Gupta <gupta.aashi3008@gmail.com>
2022-12-17 20:22:08 +0530

    second

--- app.js
+++ app.js
@@ -52,13 +52,38 @@ app.get('/options',(req,res)=>
 {
     res.render('options');
 })
+
 app.get('/originals',(req,res)=>
 {
     res.render('original');
 })
 
+
+app.get('/paintings',(req,res)=>
+{
+    res.render('paintings');
+})
+
+app.get('/potraits',(req,res)=>
+{
+    res.render('potraits');
+})
+
+
+app.get('/sculptors',(req,res)=>
+{
+    res.render('sculptors');
+})
+
+
+app.get('/fine-arts',(req,res)=>
+{
+    res.render('fine-arts');
+})
+
+
 app.post("/login", (req, res) => {    
-    const { name, email, password, password_confirm } = req.body
+    const { name, email, password, password_confirm, images } = req.body
 
     db.query('SELECT email FROM user WHERE email = ?', [email], async (error, result) => {
         if(error){
@@ -71,7 +96,7 @@ app.post("/login", (req, res) => {
             })
         } else if(password !== password_confirm) {
             return res.render('login', {
-                message: 'This email is already in use'
+                message: 'This password is already in use'
             })
         }
 
@@ -79,17 +104,29 @@ app.post("/login", (req, res) => {
 
         console.log(hashedPassword)
        
-        db.query('INSERT INTO user SET?', {name: name, email: email, password: hashedPassword}, (err, result) => {
+        db.query('INSERT INTO user SET?', {name: name, email: email, password: hashedPassword, images: images}, (err, result) => {
             if(error) {
                 console.log(error)
             } else {
-                return res.render('options', {
-                    message: 'User registered!'
+                return res.render('thank', {
+                    message: 'Your order would be deliverd soon!!!'
                 })
             }
         })        
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -107,7 +144,7 @@ app.post("/Sign-up", (req, res) => {
             })
         } else if(password !== password_confirm) {
             return res.render('Sign-up', {
-                message: 'This email is already in use'
+                message: 'This password is already in use'
             })
         }
 
@@ -126,6 +163,78 @@ app.post("/Sign-up", (req, res) => {
         })        
     })
 })
+
+
+app.post("/originals_submit", (req, res) => {    
+    const { name, email, password, password_confirm,images } = req.body
+
+    db.query('SELECT email FROM user WHERE email = ?', [email], async (error, result) => {
+        if(error){
+            console.log(error)
+        }
+
+        if( result.length > 0 ) {
+            return res.render('Originals', {
+                message: 'This email is already in use'
+            })
+        } else if(password !== password_confirm) {
+            return res.render('Originals', {
+                message: 'This password is already in use'
+            })
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8)
+
+        console.log(hashedPassword)
+       
+        db.query('INSERT INTO user SET?', {name: name, email: email, password: hashedPassword,images: images}, (err, result) => {
+            if(error) {
+                console.log(error)
+            } else {
+                return res.render('Originals', {
+                    message: 'sucessful'
+                })
+            }
+        })        
+    })
+})
+
+// app.post('/originals_submit', (req, res) => {
+//     const {email, images} = req.body;
+//     db.query('SELECT * FROM user WHERE email = ?', [req.params.email], (err, rows) => {
+//         if (!err) {
+//             res.send(rows)
+//             db.query('INSERT INTO user SET ? where email = email',  images, (err, rows) => {
+     
+//             if (!err) {
+//                 res.send(`Beer with the record ID  has been added.`)
+//             } else {
+//                 console.log(err)
+//             }
+            
+//             console.log('The data from beer table are:11 \n', images)
+    
+//             })
+
+//         } else {
+//             console.log(err)
+//         }
+        
+      
+//     })
+        
+//     })
+// ;
+
+
+
+
+
+
+
+
+
+
 
 app.listen(5000, ()=> {
     console.log("server started on port 5000")
